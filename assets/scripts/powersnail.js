8371ce367da24fa8b482b3e9a20fed9c1e2930c2
Saptarshi Laha <saptarshi.laha@gmail.com>
2022-01-27 22:28:30 +0000

    Added Variable Handling Structure (Testing & Finishing Up Pending).

--- assets/scripts/powersnail.js
+++ assets/scripts/powersnail.js
@@ -11,6 +11,9 @@ function powerlessSnail(Data){
 
     var escapes = [];
     var stack = [];
+    var variables = [];
+    var scopes = [];
+    var functions = [];
 
     var escaped = false;
     var inquotes1 = false;
@@ -19,15 +22,16 @@ function powerlessSnail(Data){
     var semicolon = false;
     var comment = false;
     var multilinecomment = false;
-    var athere = false;
+    var athere1 = false;
+    var athere2 = false;
 
     for(var x = 0; x < Data.length; x++){
 
-        if(Data.charAt(x) == "`" && escapes.length == 0 && inquotes1 == false && inquotes2 == false && comment == false && multilinecomment == false && athere == false){
+        if(Data.charAt(x) == "`" && escapes.length == 0 && inquotes1 == false && inquotes2 == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             escapes.push("`");
             escaped = true;
         }
-        else if(Data.charAt(x) == "\"" && inquotes2 == false && variable == false && escaped == false && comment == false && multilinecomment == false && athere == false){
+        else if(Data.charAt(x) == "\"" && inquotes2 == false && variable == false && escaped == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             if((stack.length-1) >=0 && stack[stack.length - 1] == "\"" && Data.charAt(x-1) != "@" && Data.charAt(x+1) != "@"){
                     stack.pop();
                     inquotes1 = false;
@@ -41,7 +45,7 @@ function powerlessSnail(Data){
                     inquotes1 = true;
             }
         }
-        else if(Data.charAt(x) == "\'" && inquotes1 == false && variable == false && escaped == false && comment == false && multilinecomment == false && athere == false){
+        else if(Data.charAt(x) == "\'" && inquotes1 == false && variable == false && escaped == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             if((stack.length-1) >=0 && stack[stack.length - 1] == "\'" && Data.charAt(x-1) != "@" && Data.charAt(x+1) != "@"){
                     stack.pop();
                     inquotes2 = false;
@@ -55,10 +59,10 @@ function powerlessSnail(Data){
                     inquotes2 = true;
             }
         }
-        else if(Data.charAt(x) == "$" && escaped == false && variable == false && inquotes1 == false && inquotes2 == false && comment == false && Data.charAt(x+1) == "{" && multilinecomment == false && athere == false){
+        else if(Data.charAt(x) == "$" && escaped == false && variable == false && inquotes1 == false && inquotes2 == false && comment == false && Data.charAt(x+1) == "{" && multilinecomment == false && athere1 == false && athere2 == false){
                 variable = true;
         }
-        else if(Data.charAt(x) == "{" && escaped == false && inquotes1 == false && inquotes2 == false && variable == true && comment == false && multilinecomment == false && athere == false){
+        else if(Data.charAt(x) == "{" && escaped == false && inquotes1 == false && inquotes2 == false && variable == true && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             stack.push("{");
             if(stack.includes("{")){
                 variable = true;
@@ -67,7 +71,7 @@ function powerlessSnail(Data){
                 variable = false;
             }
         }
-        else if(Data.charAt(x) == "}" && escaped == false && inquotes1 == false && inquotes2 == false && variable == true && comment == false && multilinecomment == false && athere == false){
+        else if(Data.charAt(x) == "}" && escaped == false && inquotes1 == false && inquotes2 == false && variable == true && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             if(stack[stack.length -1] == "{"){
             stack.pop();    
             }            
@@ -78,65 +82,222 @@ function powerlessSnail(Data){
                 variable = false;
             }
         }
-        else if(Data.charAt(x) == ";" && escaped == false && inquotes1 == false && inquotes2 == false && variable == false && comment == false && multilinecomment == false && athere == false){
+        else if(Data.charAt(x) == ";" && escaped == false && inquotes1 == false && inquotes2 == false && variable == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             semicolon = true;
         }
-        else if(Data.charAt(x) == "#" && escaped == false && inquotes1 == false && inquotes2 == false && variable == false && multilinecomment == false && comment == false && athere == false){
+        else if(Data.charAt(x) == "#" && escaped == false && inquotes1 == false && inquotes2 == false && variable == false && multilinecomment == false && comment == false && athere1 == false && athere2 == false){
             comment = true;
         }
-        else if(Data.charAt(x) == "<" && Data.charAt(x+1) == "#" && escaped == false && inquotes1 == false && inquotes2 == false && variable == false && comment == false && multilinecomment == false && athere == false){
+        else if(Data.charAt(x) == "<" && Data.charAt(x+1) == "#" && escaped == false && inquotes1 == false && inquotes2 == false && variable == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             multilinecomment = true;
         }
-        else if(Data.charAt(x) == "@" && Data.charAt(x+1) == "\"" && variable == false && escaped == false && comment == false && multilinecomment == false && athere == false && inquotes1 == false && inquotes2 == false){
+        else if(Data.charAt(x) == "@" && Data.charAt(x+1) == "\"" && variable == false && escaped == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false && inquotes1 == false && inquotes2 == false){
                     stack.push("@\"");
                     x = x + 1;
-                    athere = true;
+                    athere1 = true;
         }
-        else if(Data.charAt(x) == "\"" && Data.charAt(x+1) == "@" && variable == false && escaped == false && comment == false && multilinecomment == false && athere == true && inquotes1 == false && inquotes2 == false){
+        else if(Data.charAt(x) == "\"" && Data.charAt(x+1) == "@" && variable == false && escaped == false && comment == false && multilinecomment == false && athere1 == true && inquotes1 == false && inquotes2 == false){
                     if(stack[stack.length - 1] == "@\""){
                         stack.pop();
                         x = x + 1;
                         if(stack.includes("@\"") || stack.includes("@\'")){
-                            athere = true;
+                            athere1 = true;
                         }
                         else{
-                            athere = false;
+                            athere1 = false;
                         }
                     }
         }
-        else if(Data.charAt(x) == "@" && Data.charAt(x+1) == "\'" && variable == false && escaped == false && comment == false && multilinecomment == false && athere == false && inquotes1 == false && inquotes2 == false){
+        else if(Data.charAt(x) == "@" && Data.charAt(x+1) == "\'" && variable == false && escaped == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false && inquotes1 == false && inquotes2 == false){
                     stack.push("@\'");
                     x = x + 1;
-                    athere = true;
+                    athere2 = true;
         }
-        else if(Data.charAt(x) == "\'" && Data.charAt(x+1) == "@" && variable == false && escaped == false && comment == false && multilinecomment == false && athere == true && inquotes1 == false && inquotes2 == false){
+        else if(Data.charAt(x) == "\'" && Data.charAt(x+1) == "@" && variable == false && escaped == false && comment == false && multilinecomment == false && athere2 == true && inquotes1 == false && inquotes2 == false){
                     if(stack[stack.length - 1] == "@\'"){
                         stack.pop();
                         x = x + 1;
                         if(stack.includes("@\"") || stack.includes("@\'")){
-                            athere = true;
+                            athere2 = true;
                         }
                         else{
-                            athere = false;
+                            athere2 = false;
                         }
                     }
         }
+        else if(Data.charAt(x) == "$" && Data.charAt(x+1) != "{" && variable == false && escaped == false && comment == false && multilinecomment == false  && inquotes1 == false && inquotes2 == false && athere2 == false){
+            x = x + 1;
+            var y = x;
+            var z = y;
+            var complexVar = false;
+            var modifications = 0;
+            while(true){
+                if(Data.charAt(x) == ":" && complexVar == false){
+                    var scope = Data.slice(y, x);
+                    if(scopes.includes(scope) == false && LC_scopeVars.includes(scope.toLowerCase()) == false){
+                        modifications = 1;
+                        scopes.push(scope);
+                    }
+                    else if(LC_scopeVars.includes(scope.toLowerCase()) == true){
+                        modifications = 1;
+                        scopes.push("LC_scopeVars$" + LC_scopeVars.indexOf(scope));
+                    }
+                    else if(scopes.includes(scope)){
+                        modifications = 1;
+                        scopes.push("scopes$" + scopes.indexOf(scope));
+                    }
+                    y = x + 1;
+                }
+                else if(Data.charAt(x) == "{"){
+                    complexVar = true;
+                }
+                else if(Data.charAt(x) == "}" && complexVar == true){
+                    complexVar = false;
+                    var variable = Data.slice(y, x+1);
+                    if(variables.includes(variable) == false && LC_automaticVars.includes(variable.toLowerCase()) == false && LC_preferenceVars.includes(variable.toLowerCase()) == false){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push(variable);
+                    }
+                    else if(LC_automaticVars.includes(variable.toLowerCase()) == true){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push("LC_automaticVars$" + LC_automaticVars.indexOf(variable));
+                    }
+                    else if(LC_preferenceVars.includes(variable.toLowerCase()) == true){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push("LC_preferenceVars$" + LC_preferenceVars.indexOf(variable));
+                    }
+                    else if(variables.includes(variable)){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push("variables$" + variables.indexOf(variable));         
+                    }
+                }
+                else if(/[A-Za-z0-9_$?^]/.test(Data.charAt(x)) == false && complexVar == false){
+                    var variable = Data.slice(y, x+1);
+                   var variable = Data.slice(y, x+1);
+                    if(variables.includes(variable) == false && LC_automaticVars.includes(variable.toLowerCase()) == false && LC_preferenceVars.includes(variable.toLowerCase()) == false){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push(variable);
+                    }
+                    else if(LC_automaticVars.includes(variable.toLowerCase()) == true){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push("LC_automaticVars$" + LC_automaticVars.indexOf(variable));
+                    }
+                    else if(LC_preferenceVars.includes(variable.toLowerCase()) == true){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push("LC_preferenceVars$" + LC_preferenceVars.indexOf(variable));
+                    }
+                    else if(variables.includes(variable)){
+                        if(modifications == 0){
+                            modifications = 2;
+                        }
+                        else if(modifications == 1){
+                            modifications = 3;
+                        }
+                        variables.push("variables$" + variables.indexOf(variable));         
+                    }
+                }
+
+                if(modifications == 2){
+                    scopes.push("");
+                    if(variables[variables.length - 1].includes("LC_automaticVars$")){
+                        break;
+                    }
+                    else if(variables[variables.length - 1].includes("LC_preferenceVars$")){
+                        break;
+                    }
+                    else if(variables[variables.length - 1].includes("variables$")){
+                        var variable = "var" + variables[variables.length - 1].split("$")[1];
+                        Data = Data.slice(0,y) + variable + Data.slice(x+1);
+                        break;
+                    }
+                    else{
+                        var variable = "var" + (variables.length - 1);
+                        Data = Data.slice(0,y) + variable + Data.slice(x+1);
+                        break;
+                    }
+                }
+                else if(modifications == 3){                   
+                    if(){
+
+                    }
+                    else if(){
+
+                    }
+                    else{
+                        
+                    }
+
+                    if(variables[variables.length - 1].includes("LC_automaticVars$")){
+                        break;
+                    }
+                    else if(variables[variables.length - 1].includes("LC_preferenceVars$")){
+                        break;
+                    }
+                    else if(variables[variables.length - 1].includes("variables$")){
+
+                        break;
+                    }
+                    else{
+
+                        break;
+                    }
+                }
+
+                x++;
+            }
+        }
 
 
-        if(escaped == true && inquotes1 == false && inquotes2 == false && comment == false && multilinecomment == false && athere == false){
+
+        if(escaped == true && inquotes1 == false && inquotes2 == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             Data = Data.slice(0, x) + Data.slice(x+2);
             x -= 1;
             escapes.pop();
             escaped = false;
         }
 
-        if(semicolon == true && variable == false && inquotes1 == false && inquotes2 == false && comment == false && multilinecomment == false && athere == false){
+        if(semicolon == true && variable == false && inquotes1 == false && inquotes2 == false && comment == false && multilinecomment == false && athere1 == false && athere2 == false){
             Data = Data.slice(0, x+1) + "\n" + Data.slice(x+1);
             x += 1;
             semicolon = false;
         }
 
-        if(comment == true && variable == false && inquotes1 == false && inquotes2 == false && escaped == false && multilinecomment == false && athere == false){
+        if(comment == true && variable == false && inquotes1 == false && inquotes2 == false && escaped == false && multilinecomment == false && athere1 == false && athere2 == false){
             var y = x;
             while(Data.charAt(x) != "\n"){
                 x++;
@@ -146,7 +307,7 @@ function powerlessSnail(Data){
             comment = false;
         }
 
-        if(multilinecomment == true && variable == false && inquotes1 == false && inquotes2 == false && escaped == false && comment == false && athere == false){
+        if(multilinecomment == true && variable == false && inquotes1 == false && inquotes2 == false && escaped == false && comment == false && athere1 == false && athere2 == false){
             x = x + 2
             var y = x - 2;
             while(true){
@@ -185,7 +346,7 @@ function powerlessSnail(Data){
     }
 
     if(balance != ""){
-    Data = Data.slice(0, Data.length - 1);
+    Data = Data.slice(0, Data.length);
     Data = Data + balance;
     }
 
@@ -201,9 +362,7 @@ function powerSnail(){
 
     if(document.getElementById("inputbox").value != ""){
         inputData = document.getElementById("inputbox").value;
-        inputData = inputData + "\n";
-
-
+        
         inputData = powerlessSnail(inputData);
         document.getElementById("outputbox").value = inputData;
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Working on the following now -
-1. {} Beautification
-2. Replacing variables inside strings
+1. Finish Variables and Test Variables.
+2. {} Beautification
 3. Replacing functions inside strings safely
 4. Base64encode/decode
 5. RawInflate/Deflate
